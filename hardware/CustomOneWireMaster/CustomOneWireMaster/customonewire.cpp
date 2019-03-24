49840339d9cf15ffd11bf54884e9abf903e6fa60
Julien <nerradia@gmail.com>
2019-03-24 05:43:49 +0100

    New bit encoding attempt

--- hardware/CustomOneWireMaster/CustomOneWireMaster/customonewire.cpp
+++ hardware/CustomOneWireMaster/CustomOneWireMaster/customonewire.cpp
@@ -9,6 +9,7 @@ customonewire::customonewire(int pin_, int freq_khz) {
 
 int customonewire::readByte(unsigned char* data, unsigned long int timeout) {
   pinMode(pin, INPUT); // Listen to line
+  *data = 0;
 
   while (digitalReadFast(pin)) 
     if (timeout--) {
@@ -17,30 +18,45 @@ int customonewire::readByte(unsigned char* data, unsigned long int timeout) {
       return 0;
     }
 
-  int bit_time = 0;
-  while (digitalReadFast(pin)) {
+  int seuil = 0;
+  while (!digitalReadFast(pin)) { // Mesure du temps bas
     delayMicroseconds(1);
-    bit_time++;
+    seuil++;
   }
-  bit_time /= 8;
 
-  delayMicroseconds(bit_time * 2); // Wait start bit + half a bit to sample on the middle of the signal
+  int bit_time;
   for (int i = 7; i >= 0; i--) {
-    *data |= digitalReadFast(pin) << i;
-    delayMicroseconds(bit_time);
+    bit_time = 0;  
+    while (digitalReadFast(pin)) { // Mesure du temps haut
+      delayMicroseconds(1);
+      bit_time++;
+    }
+
+    if (bit_time > seuil) { // Temps haut > 1.5 temps, donc bit à 1
+      *data |= 1 << i;
+    }
+
+    while(!digitalReadFast(pin)) ; // Attente de la remontée du signal pour le prochain bit
   }
+  Serial.printf("dernier bit_time : %d, seuil : %d\r\n", bit_time, seuil);
 };
 
 int customonewire::sendByte(unsigned char data) {
 
   writeBit(0);
-  delayMicroseconds(wait_us * 8);
-  writeBit(1);
-  delayMicroseconds(wait_us * 1.5);
+  delayMicroseconds(wait_us*1.5); // Temps bas pendant 1 cycle et demi (ça donne le temps à utiliser pour le seuil)
 
   for (int i = 0; i < 8; i++) {
-    writeBit((data << i) & (1 << 7));
-    delayMicroseconds(wait_us);
+    writeBit(1);
+    if ((data << i) & (1 << 7)) {
+      delayMicroseconds(wait_us*2);  // Bit à un = signal haut pendant deux temps
+    }
+    else { 
+      delayMicroseconds(wait_us); // Bit à 0 = signal haut pendant un temps
+    }
+
+    writeBit(0);
+    delayMicroseconds(wait_us); // Un temps mort à 0
   }
   pinMode(pin, INPUT); // Release line
 };
