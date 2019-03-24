8ccae0c03f843b656c6d86a40c051e47d7b6c93b
Julien <nerradia@gmail.com>
2019-03-24 04:45:28 +0100

    Better sync

--- hardware/CustomOneWireMaster/CustomOneWireMaster/customonewire.cpp
+++ hardware/CustomOneWireMaster/CustomOneWireMaster/customonewire.cpp
@@ -17,16 +17,25 @@ int customonewire::readByte(unsigned char* data, unsigned long int timeout) {
       return 0;
     }
 
-  delayMicroseconds(wait_us * 2); // Wait start bit + half a bit to sample on the middle of the signal
+  int bit_time = 0;
+  while (digitalReadFast(pin)) {
+    delayMicroseconds(1);
+    bit_time++;
+  }
+  bit_time /= 8;
+
+  delayMicroseconds(bit_time * 2); // Wait start bit + half a bit to sample on the middle of the signal
   for (int i = 7; i >= 0; i--) {
     *data |= digitalReadFast(pin) << i;
-    delayMicroseconds(wait_us);
+    delayMicroseconds(bit_time);
   }
 };
 
 int customonewire::sendByte(unsigned char data) {
 
   writeBit(0);
+  delayMicroseconds(wait_us * 8);
+  writeBit(1);
   delayMicroseconds(wait_us * 1.5);
 
   for (int i = 0; i < 8; i++) {
